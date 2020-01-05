49344dff831e953aea83bd58be81e653a99996bd
Ferran <ferran.9908@gmail.com>
2020-01-05 13:06:12 +0530

    Add colorMap to plot

--- RandomWalkPlot.py
+++ RandomWalkPlot.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 from RandomWalk import RandomWalk
 
+
 rw = RandomWalk()
 rw.fillWalk()
+numPoints = list(range(rw.num_points))
 
-plt.scatter(rw.x_values,rw.y_values,s = 1)
+plt.scatter(rw.x_values,rw.y_values,c=numPoints,cmap = plt.cm.Blues ,edgecolors='none',s = 1)
 plt.show()
