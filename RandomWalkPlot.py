5a0c96867cdb8aef8328a975d4d0428aa184884d
Ferran <ferran.9908@gmail.com>
2020-01-05 13:12:27 +0530

    Stylize Plot

--- RandomWalkPlot.py
+++ RandomWalkPlot.py
@@ -7,4 +7,11 @@ rw.fillWalk()
 numPoints = list(range(rw.num_points))
 
 plt.scatter(rw.x_values,rw.y_values,c=numPoints,cmap = plt.cm.Blues ,edgecolors='none',s = 1)
+plt.scatter(0,0,c='green',edgecolors='none',s=10)
+plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=10)
+
+# Remove the axes.
+plt.axes().get_xaxis().set_visible(False)
+plt.axes().get_yaxis().set_visible(False)
+
 plt.show()
